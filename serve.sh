3a1c27254ba4d1f9f23e640b222aa6334f418425
Fabian Peter Hammerle <fabian@hammerle.me>
2019-04-07 02:03:04 +0200

    added todo: log to stdout

--- serve.sh
+++ serve.sh
@@ -4,8 +4,12 @@ function set_option {
     (set -x; postconf -evv "$1=$2")
 }
 
+# http://www.postfix.org/postconf.5.html
 [ -z "$POSTFIX_RELAYHOST" ] || set_option relayhost "$POSTFIX_RELAYHOST"
 [ -z "$POSTFIX_RELAY_DOMAINS" ] || set_option relay_domains "$POSTFIX_RELAY_DOMAINS"
+# TODO log to stdout (requires postfix >= 3.4)
+# http://www.postfix.org/announcements/postfix-3.4.0.html
+# http://www.postfix.org/MAILLOG_README.html
 set_option syslog_name "$(hostname)/pstfx"
 
 set -x
