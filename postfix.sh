3b0312ed157b2e035088e88c2a3f0151b1ff7a3b
Fabian Peter Hammerle <fabian@hammerle.me>
2019-08-10 08:21:22 +0200

    postmap paths in $POSTMAP_PATHS

--- postfix.sh
+++ postfix.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-set -ex
+set -e
+
+echo "$POSTMAP_PATHS" | while IFS= read -r postmap_path; do
+    [ -z "$postmap_path" ] || (set -x; postmap "$postmap_path")
+done
+
+set -x
 
 # http://www.postfix.org/postconf.5.html#alias_database
 # http://www.postfix.org/newaliases.1.html
