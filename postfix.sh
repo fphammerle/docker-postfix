6ac52478284fa7a66874248d591344c1815c24a9
Fabian Peter Hammerle <fabian@hammerle.me>
2019-08-10 08:20:36 +0200

    update alias database

--- postfix.sh
+++ postfix.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# http://www.postfix.org/postconf.5.html#alias_database
+# http://www.postfix.org/newaliases.1.html
+newaliases
+
 # http://www.postfix.org/MAILLOG_README.html
 postconf -e maillog_file=/dev/stdout
 
